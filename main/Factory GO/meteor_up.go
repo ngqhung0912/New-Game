2b59492df55ea1431aac081bc829bc809fc64b9e
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2020-09-15 12:13:11 +0700

    commit #3

---
+++ main/Factory GO/meteor_up.go
@@ -0,0 +1,35 @@
+components {
+  id: "meteor_up"
+  component: "/Script/meteor_up.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/Assets/asset.atlas\"\n"
+  "default_animation: \"asteroid\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 2.0
+    y: -50.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
