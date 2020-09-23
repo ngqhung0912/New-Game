2b7e62da34165932d0e31e9daf394e6c79e104fc
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2020-09-23 14:21:46 +0700

    14:21 23/09/2020 Commit

--- main/Factory GO/meteor_above.go
+++ main/Factory GO/meteor_above.go
@@ -1,6 +1,6 @@
 components {
-  id: "meteor_up"
-  component: "/Script/meteor_up.script"
+  id: "meteor"
+  component: "/Script/meteor.script"
   position {
     x: 0.0
     y: 0.0
@@ -34,21 +34,23 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "collisionobject"
+  id: "meteor_co"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
-  "mass: 1.0\n"
-  "friction: 0.1\n"
-  "restitution: 0.5\n"
-  "group: \"asteroid\"\n"
+  "mass: 0.5\n"
+  "friction: 0.0\n"
+  "restitution: 1.0\n"
+  "group: \"meteor\"\n"
   "mask: \"wall\"\n"
+  "mask: \"ship\"\n"
+  "mask: \"meteor\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
   "      x: 2.0\n"
-  "      y: -8.0\n"
+  "      y: -11.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -60,10 +62,10 @@ embedded_components {
   "    index: 0\n"
   "    count: 1\n"
   "  }\n"
-  "  data: 83.917\n"
+  "  data: 91.528\n"
   "}\n"
   "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
+  "angular_damping: 1.0\n"
   "locked_rotation: false\n"
   ""
   position {
